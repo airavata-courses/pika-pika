34af778843091e791c4929e03d08e9f17f3a6228
Akshay <aksrajvanshi@gmail.com>
2020-02-02 16:13:18 -0500

    Adding pom dependency for netCDF and added netcdf file handling code.

---
+++ post-processing/src/main/java/postprocessing/NetCDFHandler.java
@@ -0,0 +1,95 @@
+package postprocessing;
+
+import ucar.nc2.constants.FeatureType;
+import ucar.nc2.dt.RadialDatasetSweep;
+import ucar.nc2.dt.TypedDatasetFactory;
+import ucar.nc2.util.CancelTask;
+
+public class NetCDFHandler {
+
+    public static void main(String[] args) {
+
+	      try {
+
+	        String fileIn = "KPAH20080330_133313_V03";
+	      
+	        CancelTask emptyCancelTask = new CancelTask() {
+	            public boolean isCancel() {
+	                return false;
+	            }
+	            public void setError(String arg0) {
+	            }
+				@Override
+				public void setProgress(String arg0, int arg1) {
+					// TODO Auto-generated method stub
+					
+				}
+	        };
+	       
+	        
+	        
+	        // open the file and represent as a 
+	        // RadialDatasetSweep object        
+	        RadialDatasetSweep rds = (RadialDatasetSweep)
+	            TypedDatasetFactory.open(
+	               FeatureType.RADIAL, 
+	               fileIn, 
+	               emptyCancelTask,
+	               new StringBuilder()
+	            );
+
+
+	        // radar information
+	        String stationID      = rds.getRadarID();
+	        String stationName    = rds.getRadarName();
+	        boolean isVolume       = rds.isVolume();
+
+	        System.out.println("stationID = "+stationID);
+	        System.out.println("stationName = "+stationName);
+	        System.out.println("isVolume = "+isVolume);
+	        System.out.println("station location = "+
+	            rds.getCommonOrigin());
+
+
+
+	        // Read a radial variable
+	        RadialDatasetSweep.RadialVariable varRef =
+	            (RadialDatasetSweep.RadialVariable) 
+	               rds.getDataVariable("Reflectivity");
+
+
+	        // Read a single sweep
+	        int sweepNum = 0;
+	        RadialDatasetSweep.Sweep sweep = 
+	            varRef.getSweep(sweepNum);
+
+	        float meanElev = sweep.getMeanElevation();
+	        int nrays = sweep.getRadialNumber();
+	        float beamWidth = sweep.getBeamWidth();
+	        int ngates = sweep.getGateNumber();
+	        float gateSize = sweep.getGateSize();
+
+	        System.out.println("meanElev = "+meanElev);
+	        System.out.println("nrays = "+nrays);
+	        System.out.println("beamWidth = "+beamWidth);
+	        System.out.println("ngates = "+ngates);
+	        System.out.println("gateSize = "+gateSize);
+
+
+	         // Read data variable at radial level - 
+	         // this is where actual data is read 
+	         // into memory
+	         for (int i = 0; i < nrays; i++) {
+	             float azimuth = sweep.getAzimuth(i);
+	             float elevation = sweep.getElevation(i);
+	             float[] data = sweep.readData(i);
+	             // data.length should equal ngates
+
+	         }
+	         
+	      } catch (Exception e) {
+	          e.printStackTrace();
+	      }
+    }
+
+}
