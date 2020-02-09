75eef2d16a8d98e14ddd697beee222f928bdd055
sharavi <sharavi@iu.edu>
2020-02-09 13:54:55 -0500

    kafka integration with model execution

--- model-execution-microservice/modelenv/consumer.py
+++ model-execution-microservice/modelenv/consumer.py
@@ -1,9 +1,25 @@
 from pykafka import KafkaClient
+import subset
+import json
+
+
+client = KafkaClient(hosts="127.0.0.1:9092")
+
+topic =client.topics['postprocess']
+producer = topic.get_sync_producer()
 
 def get_kafka_client():
     return KafkaClient(hosts='127.0.0.1:9092')
-client = get_kafka_client()
+# client = get_kafka_client()
 def events():
-    for i in client.topics['testFrontEnd'].get_simple_consumer():
-        print('data:{0}\n\n'.format(i.value.decode()))
+	
+	# print(bytes(subset_data[0]))
+	#producer.produce(message.encode('ascii'))
+	# print(len(subset_data))
+	for i in client.topics['model-execution-service'].get_simple_consumer():
+		print('data:{0}\n\n'.format(i.value.decode()))
+		data=json.loads(i.value.decode())
+		subset_data=subset.subset(data['bucket'],data['key'])
+		# print(subset_data)
+		producer.produce(bytes('{"url":"'+subset_data+'"}','ascii'))	
 events()
