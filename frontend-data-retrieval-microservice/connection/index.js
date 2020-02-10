7324113d0c3720e6e0b848911abc8a9e83434b9a
sharavi <sharavi@iu.edu>
2020-02-10 02:52:35 -0500

    adding kafka to front end data retrieval microservice

--- frontend-data-retrieval-microservice/connection/index.js
+++ frontend-data-retrieval-microservice/connection/index.js
@@ -3,27 +3,28 @@ const AWS = require('aws-sdk');
 const Producer = kafka.Producer;
 const Consumer = kafka.Consumer;
 const client = new kafka.KafkaClient();
-let producer=null
-let consumer=null
+let producer = null
+let consumer = null
 
 try {
-    producer = new Producer(client);
-    consumer = new Consumer(
-        client,
-        [{ topic: 'data-retrieval-service', offset:1,partition: 1 }],
-        {
-          autoCommit: false,
-          fetchMaxWaitMs: 1000,
-          fetchMaxBytes: 1024000000,
-          encoding: 'utf8',
-          fromOffset: true
-        }
-      )
-    AWS.config.update({
-        'accessKeyId': 'AKIAITP4A4YJNLYIUYFA',
-        'secretAccessKey': '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'})
-  
+	producer = new Producer(client);
+	consumer = new Consumer(
+		client,
+		[{ topic: 'front-end-data-retrieval-service', offset: 12, partitions: 1 }],
+		{
+			autoCommit: false,
+			fetchMaxWaitMs: 1000,
+			fetchMaxBytes: 1024000000,
+			encoding: 'utf8',
+			fromOffset: true
+		}
+	)
+	AWS.config.update({
+		'accessKeyId': 'AKIAITP4A4YJNLYIUYFA',
+		'secretAccessKey': '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'
+	})
+
 } catch (error) {
-    console.log(error)
+	console.log(error)
 }
-module.exports={producer,consumer,AWS}
+module.exports = { producer, consumer, AWS }
--- frontend-data-retrieval-microservice/index.js
+++ frontend-data-retrieval-microservice/index.js
@@ -1,53 +1,43 @@
-const express = require("express");
-const path = require("path");
-const bodyParser = require("body-parser");
-const request = require("request");
 const consumer = require("./connection").consumer;
 const utility = require("./utility");
+var sendPayload = require('./utility/index').sendPayload
 
-const PORT = 4000;
 
-const app = express();
-app.use(bodyParser.json());
+let functionMap = {
+	'radar': utility.fetchRadar,
+	'nexradfiles': utility.fetchNEXRADFiles,
+	'weather': utility.fetchCurrentWeather
+}
+
 
 consumer.on("message", message => {
-  console.log("Message on Topic -> " + message.topic);
-  console.log(message.value);
-  let NEXRADKey = JSON.parse(message.value)["key"];
+	console.log("Message on Topic -> " + message.topic);
+	let value = JSON.parse(message.value)
+	functionMap[value['key']](value['data']).then((data) => {
+		payloadMessage = {
+			error: null,
+			data: data,
+			resId: value['resId']
+		}
+		sendPayload('api-gateway-service', JSON.stringify(payloadMessage))
+			.catch((error) => {
+				console.log(error)
+			})
+
+	}).catch((error) => {
+
+		console.log(error)
+		payloadMessage = {
+			error: error,
+			data: null,
+			resId: value['resId']
+		}
+		sendPayload('api-gateway-service', JSON.stringify(payloadMessage))
+			.catch((error) => {
+				console.log(error)
+			})
+	})
 });
 consumer.on("error", error => {
-  console.log("error", error);
+	console.log("error", error);
 });
-// utility
-//   .fetchNEXRAD("2020/02/05")
-//   .then(value => {
-//     console.log("Data Submitted!");
-//   })
-//   .catch(error => {
-//     console.error(error);
-//   });
-
-//let date = JSON.parse(message.value)["date"];
-//let radar = JSON.parse(message.value)["radar"];
-
-// let date = "2020/02/05";
-// let radar = "KAMX";
-// utility
-//   .fetchNEXRADFiles(date, radar)
-//   .then(value => {
-//     console.log("Data Submitted!");
-//   })
-//   .catch(error => {
-//     console.error(error);
-//   });
-
-let lat = "30.30";
-let lon = "-98.03";
-utility
-  .fetchCurrentWeather(lat, lon)
-  .then(value => {
-    console.log(value.data.main);
-  })
-  .catch(error => {
-    console.error(error);
-  });
