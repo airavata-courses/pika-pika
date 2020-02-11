c9f46f9e3876130082e71dcd44b8badb046bc5c9
pokearu <aramali@iu.edu>
2020-02-10 23:22:54 -0500

    Consumer changes

--- model-execution-microservice/modelenv/consumer.py
+++ model-execution-microservice/modelenv/consumer.py
@@ -2,10 +2,9 @@ from pykafka import KafkaClient
 import subset
 import json
 
-
 client = KafkaClient(hosts="127.0.0.1:9092")
 
-topic =client.topics['postprocess']
+topic =client.topics['post-processing-service']
 producer = topic.get_sync_producer()
 
 def get_kafka_client():
@@ -16,10 +15,18 @@ def events():
 	# print(bytes(subset_data[0]))
 	#producer.produce(message.encode('ascii'))
 	# print(len(subset_data))
+	
 	for i in client.topics['model-execution-service'].get_simple_consumer():
-		print('data:{0}\n\n'.format(i.value.decode()))
+		print('data : {0}\n\n'.format(i.value.decode()))
 		data=json.loads(i.value.decode())
 		subset_data=subset.subset(data['bucket'],data['key'])
-		# print(subset_data)
-		producer.produce(bytes('{"url":"'+subset_data+'"}','ascii'))	
+		try:
+			result=data
+			result["url"]=subset_data
+			# result={"jobID":data["jobID"],"bucket":data['bucket']
+			# ,"key":data['key'],"url":subset_data}
+			result=json.dumps(result)
+			producer.produce(bytes(result,'utf-8'))
+		except Exception as e:
+			print(str(e))
 events()
