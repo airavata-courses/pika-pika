812dc47d47f4068223573553e5ae7c7af4dd7b13
sharavi <sharavi@iu.edu>
2020-02-12 19:05:49 -0500

    Get Results function

--- session-management-microservice/utility/index.js
+++ session-management-microservice/utility/index.js
@@ -14,7 +14,6 @@ const updateJobData = (jobData) => {
 }
 
 const updateResult = (resultData) => {
-	console.log(resultData)
 	if (resultData['data']['key'] == 'fetch') {
 		return fetchResult(resultData)
 	}
