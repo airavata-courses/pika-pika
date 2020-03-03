c6f893177b1e2467e7a84eb89ec353f54ba320ae
Akshay <aksrajvanshi@gmail.com>
2020-03-03 14:05:21 -0500

    Changed back to original version of data link retrieval service.

--- DataLinkRetrieve/src/main/java/ProducerConsumerFiles/MessageListener.java
+++ DataLinkRetrieve/src/main/java/ProducerConsumerFiles/MessageListener.java
@@ -1,17 +1,12 @@
 package ProducerConsumerFiles;
 
 import DataLinkRetrieve.DataLinkRetrieveApplication;
-import lombok.extern.log4j.Log4j2;
-import org.apache.log4j.LogManager;
-import org.apache.log4j.Logger;
 import org.springframework.kafka.annotation.KafkaListener;
 
 import java.util.concurrent.CountDownLatch;
 
-@Log4j2
 public class MessageListener {
 
-    Logger logger = LogManager.getLogger(MessageListener.class);
     private CountDownLatch latch = new CountDownLatch(3);
 
     public CountDownLatch getLatch() {
@@ -21,7 +16,6 @@ public class MessageListener {
     @KafkaListener(topics = "#{'${consumer.topic.name}'}", groupId = "test-consumer-group")
     public void listen(String message) {
         DataLinkRetrieveApplication.producer.sendMessage(message);
-        logger.info("Producer is producing message : " + message);
         latch.countDown();
     }
 }
--- DataLinkRetrieve/src/main/java/ProducerConsumerFiles/MessageProducer.java
+++ DataLinkRetrieve/src/main/java/ProducerConsumerFiles/MessageProducer.java
@@ -1,8 +1,5 @@
 package ProducerConsumerFiles;
 
-import lombok.extern.log4j.Log4j2;
-import org.apache.log4j.LogManager;
-import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.core.KafkaTemplate;
@@ -10,14 +7,10 @@ import org.springframework.kafka.support.SendResult;
 import org.springframework.util.concurrent.ListenableFuture;
 import org.springframework.util.concurrent.ListenableFutureCallback;
 
-@Log4j2
 public class MessageProducer {
 
     @Autowired
     private KafkaTemplate<String, String> kafkaTemplate;
-
-    Logger logger = LogManager.getLogger(MessageProducer.class);
-
     @Value(value = "${producer.topic.name}")
     private String topicName;
 
@@ -29,13 +22,12 @@ public class MessageProducer {
 
             @Override
             public void onSuccess(SendResult<String, String> result) {
-                logger.info("Sent message: " + message + " with offset: " + result.getRecordMetadata().offset());
+
             }
 
             @Override
             public void onFailure(Throwable ex) {
                 System.out.println("Unable to send message=[" + message + "] due to : " + ex.getMessage());
-                logger.info("Unable to send message: " + message + " due to : " + ex.getMessage());
             }
         });
     }
