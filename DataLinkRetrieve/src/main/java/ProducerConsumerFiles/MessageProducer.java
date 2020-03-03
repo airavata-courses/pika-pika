8697646e3a77372d758965a8949d90106cd2cb00
Akshay <aksrajvanshi@gmail.com>
2020-03-03 12:19:17 -0500

    Incorporated logging into the project.

--- DataLinkRetrieve/src/main/java/ProducerConsumerFiles/MessageProducer.java
+++ DataLinkRetrieve/src/main/java/ProducerConsumerFiles/MessageProducer.java
@@ -1,5 +1,8 @@
 package ProducerConsumerFiles;
 
+import lombok.extern.log4j.Log4j2;
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.core.KafkaTemplate;
@@ -7,10 +10,13 @@ import org.springframework.kafka.support.SendResult;
 import org.springframework.util.concurrent.ListenableFuture;
 import org.springframework.util.concurrent.ListenableFutureCallback;
 
+@Log4j2
 public class MessageProducer {
 
     @Autowired
     private KafkaTemplate<String, String> kafkaTemplate;
+
+    Logger logger = LogManager.getLogger(MessageProducer.class);
 
     @Value(value = "${producer.topic.name}")
     private String topicName;
@@ -23,12 +29,13 @@ public class MessageProducer {
 
             @Override
             public void onSuccess(SendResult<String, String> result) {
-                System.out.println("Sent message=[" + message + "] with offset=[" + result.getRecordMetadata().offset() + "]");
+                logger.info("Sent message: " + message + " with offset: " + result.getRecordMetadata().offset());
             }
 
             @Override
             public void onFailure(Throwable ex) {
                 System.out.println("Unable to send message=[" + message + "] due to : " + ex.getMessage());
+                logger.info("Unable to send message: " + message + " due to : " + ex.getMessage());
             }
         });
     }
