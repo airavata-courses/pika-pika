3924996ea645f08ff8a01285e9d76c3145b8293e
Akshay <aksrajvanshi@gmail.com>
2020-03-08 18:20:47 -0400

    Added logging to spring boot application.

--- DataLinkRetrieve/src/main/java/ProducerConsumerFiles/KafkaMessageListener.java
+++ DataLinkRetrieve/src/main/java/ProducerConsumerFiles/KafkaMessageListener.java
@@ -1,6 +1,8 @@
 package ProducerConsumerFiles;
 
 import Entity.MessageEntity;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
@@ -8,6 +10,7 @@ import org.springframework.stereotype.Service;
 @Service
 public class KafkaMessageListener {
 
+    private static Logger logger = LogManager.getLogger(KafkaMessageListener.class);
     public static final String TOPIC = "data-retrieval-service";
     static public MessageEntity messageEntity = new MessageEntity();
 
@@ -16,7 +19,7 @@ public class KafkaMessageListener {
 
     @KafkaListener(topics = TOPIC, groupId = "data-retrieval-group")
     public void consume(String message) {
-
+        logger.info("message produced at " + TOPIC + " : " + message);
         kafkaProducer.produce(message);
         messageEntity.setDataRetrievalData(message);
     }
--- DataLinkRetrieve/src/main/java/ProducerConsumerFiles/KafkaMessageProducer.java
+++ DataLinkRetrieve/src/main/java/ProducerConsumerFiles/KafkaMessageProducer.java
@@ -1,5 +1,7 @@
 package ProducerConsumerFiles;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
@@ -9,10 +11,13 @@ public class KafkaMessageProducer {
 
     public static final String TOPIC = "model-execution-service";
 
+    private static Logger logger = LogManager.getLogger(KafkaMessageProducer.class);
+
     @Autowired
     private KafkaTemplate<String, String> kafkaTemplate;
 
     public void produce(String message) {
+        logger.info("message produced at " + TOPIC + " : " + message);
         this.kafkaTemplate.send(TOPIC, message);
     }
 }
