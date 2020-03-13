824d264f0aa902a823d74460eada1f3561dcc542
pokearu <aramali@iu.edu>
2020-03-13 16:22:54 -0400

    Changing broker dns

--- api-gateway-microservice/connection/index.js
+++ api-gateway-microservice/connection/index.js
@@ -1,7 +1,7 @@
 const kafka = require('kafka-node')
 const Producer = kafka.Producer
 const Consumer = kafka.Consumer
-const client = new kafka.KafkaClient({kafkaHost: "kafka-service:9092"})
+const client = new kafka.KafkaClient({kafkaHost: "kafka-broker:9092"})
 let producer = null
 let consumer = null
 
@@ -11,11 +11,13 @@ try {
 		client,
 		[{ topic: 'api-gateway-service', offset: 0, partition: 0 }],
 		{
+			groupId: 'api-gateway-service',
 			autoCommit: true,
+			autoCommitIntervalMs: 5000,
 			fetchMaxWaitMs: 1000,
 			fetchMaxBytes: 1024,
 			encoding: 'utf8',
-			fromOffset: true
+			fromOffset: false
 		}
 	)
 } catch (error) {
