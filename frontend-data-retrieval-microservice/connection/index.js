560e4e7ae4e7b18d12f5510286831d44904392ce
pokearu <aramali@iu.edu>
2020-03-15 22:39:00 -0400

    Changes for secret keys and deployment

--- frontend-data-retrieval-microservice/connection/index.js
+++ frontend-data-retrieval-microservice/connection/index.js
@@ -1,8 +1,9 @@
 const kafka = require('kafka-node');
 const AWS = require('aws-sdk');
+const process=require('process')
 const Producer = kafka.Producer;
 const Consumer = kafka.Consumer;
-const client = new kafka.KafkaClient();
+const client = new kafka.KafkaClient({kafkaHost: "kafka-service:9092"});
 let producer = null
 let consumer = null
 
@@ -10,18 +11,23 @@ try {
 	producer = new Producer(client);
 	consumer = new Consumer(
 		client,
-		[{ topic: 'front-end-data-retrieval-service', offset: 0, partitions: 1 }],
+		[{ topic: 'front-end-data-retrieval-service', offset: 0, partitions: 0 }],
 		{
-			autoCommit: false,
+			groupId: 'front-end-data-retrieval-group',
+			autoCommit: true,
+			autoCommitIntervalMs: 5000,
 			fetchMaxWaitMs: 1000,
-			fetchMaxBytes: 102400,
+			fetchMaxBytes: 1024,
 			encoding: 'utf8',
-			fromOffset: true
+			fromOffset: false
 		}
 	)
+	console.log(process.env['AWSAccessKeyId'])
+	console.log(process.env['AWSSecretKey'])
+	
 	AWS.config.update({
-		'accessKeyId': '~',
-		'secretAccessKey': '~'
+		'accessKeyId': process.env['AWSAccessKeyId'],
+		'secretAccessKey': process.env['AWSSecretKey']
 	})
 
 } catch (error) {
