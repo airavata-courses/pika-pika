dcb7db1a78af8eab2882255c94ae2397fc6da021
pokearu <aramali@iu.edu>
2020-03-20 03:03:28 -0400

    Consumer group changes

--- frontend-data-retrieval-microservice/connection/index.js
+++ frontend-data-retrieval-microservice/connection/index.js
@@ -2,7 +2,7 @@ const kafka = require('kafka-node');
 const AWS = require('aws-sdk');
 const process=require('process')
 const Producer = kafka.Producer;
-const Consumer = kafka.Consumer;
+const Consumer = kafka.ConsumerGroup;
 const client = new kafka.KafkaClient({kafkaHost: "kafka-service:9092"});
 let producer = null
 let consumer = null
@@ -10,17 +10,18 @@ let consumer = null
 try {
 	producer = new Producer(client);
 	consumer = new Consumer(
-		client,
-		[{ topic: 'front-end-data-retrieval-service', offset: 0, partitions: 0 }],
 		{
+			kafkaHost: "kafka-service:9092",
 			groupId: 'front-end-data-retrieval-group',
 			autoCommit: true,
+			protocol: ["roundrobin"],
 			autoCommitIntervalMs: 5000,
 			fetchMaxWaitMs: 1000,
 			fetchMaxBytes: 1024*1024,
 			encoding: 'utf8',
-			fromOffset: false
-		}
+			fromOffset: "latest"
+		},
+		['front-end-data-retrieval-service']
 	)
 	console.log(process.env['AWSAccessKeyId'])
 	console.log(process.env['AWSSecretKey'])
